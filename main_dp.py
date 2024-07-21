6defda03457305d8f3277a68d69811ad625e37d4
ChanJae Lee <48813631+arisel117@users.noreply.github.com>
2024-07-21 18:05:45 +0900

    Create main_dp.py

---
+++ main_dp.py
@@ -0,0 +1,108 @@
+# -*- coding: utf-8 -*-
+
+import os
+import argparse
+
+import torch
+from torch.utils.data import DataLoader, TensorDataset
+
+
+def get_args_parser():
+    parser = argparse.ArgumentParser(add_help=False)
+    parser.add_argument('--lr', type=float, default=0.01)
+    parser.add_argument('--epoch', type=int, default=10)
+    parser.add_argument('--batch_size', type=int, default=10)
+    parser.add_argument('--num_workers', type=int, default=2)
+    parser.add_argument('--seed', type=int, default=42)
+    return parser
+
+
+class SimpleModel(torch.nn.Module):
+    def __init__(self):
+        super(SimpleModel, self).__init__()
+        self.linear = torch.nn.Linear(10, 10)
+
+    def forward(self, x):
+        return self.linear(x)
+
+
+def get_dataset():
+    x = torch.randn(1000, 10)
+    y = torch.randn(1000, 10)
+    dataset = TensorDataset(x, y)
+    return dataset
+
+
+def main(args):
+    torch.manual_seed(args.seed)
+
+    # load data
+    train_dataset = get_dataset()
+    train_loader = DataLoader(
+        dataset=train_dataset,
+        num_workers=int(args.num_workers),
+        batch_size=int(args.batch_size))
+    # val_loader / test_loader
+
+    # for record
+    init_start_event = torch.cuda.Event(enable_timing=True)
+    init_end_event = torch.cuda.Event(enable_timing=True)
+
+    # load model
+    model = SimpleModel()
+    model = torch.nn.DataParallel(model).cuda()
+
+    # set criterion and optimizer
+    criterion = torch.nn.CrossEntropyLoss()
+    optimizer = torch.optim.SGD(
+        params=model.parameters(),
+        lr=0.01,
+        weight_decay=0.0005,
+        momentum=0.9)
+    scheduler = torch.optim.lr_scheduler.StepLR(
+        optimizer=optimizer,
+        step_size=5,
+        gamma=0.1)
+
+    # train
+    init_start_event.record()
+    for epoch in range(1, 10 + 1):
+        model.train()
+        for data, target in train_loader:
+            data, target = data.cuda(), target.cuda()
+            optimizer.zero_grad()
+            output = model(data)
+            loss = criterion(output, target)
+            loss.backward()
+            optimizer.step()
+
+        print(f"Epoch {str(epoch).zfill(2)}, Loss: {round(loss.item(), 5)}")
+
+        # val_loader / test_loader
+        # model.eval()
+        # with torch.no_grad():
+
+        scheduler.step()
+    init_end_event.record()
+
+    print(f"CUDA event elapsed time: {init_start_event.elapsed_time(init_end_event) / 1000}sec")
+    print(f"{model}")
+
+    # save model
+    states = model.state_dict()
+    # torch.save(states, "my_model.pt")
+
+    return
+
+
+if __name__ == '__main__':
+    os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
+    os.environ["CUDA_VISIBLE_DEVICES"] = "0,1,2,3"
+
+    parser = argparse.ArgumentParser('DP Training Test', parents=[get_args_parser()])
+    args = parser.parse_args()
+
+    main(args)
+
+
+# python main_dp.py
